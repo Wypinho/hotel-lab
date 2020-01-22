727e7fa6ac7fe9a19377800c10b7c02094173d2d
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-22 13:24:44 +0000

    set up router and tested index route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,7 +5,19 @@ const createRouter = function(collection) {
 
   const router = express.Router();
 
-  
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  return router
 
 };
 
